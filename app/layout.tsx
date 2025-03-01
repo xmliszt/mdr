93654faa823bf1703780208cc4c61604dfb75858
Li Yuxuan <xmliszt@gmail.com>
2025-03-01 13:30:18 +0800

    [Lumon] Fix eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import { VT323 } from "next/font/google";
-import { cn } from "@/lib/utils";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Lumon - MDR",
